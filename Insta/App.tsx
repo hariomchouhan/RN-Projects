3663357176d593b8619cfe5dae9a9bd645070db5
Hariom <hariomchouhan430@gmail.com>
2024-11-14 13:58:30 +1030

    install all dependencies for navigation in react native

--- Insta/App.tsx
+++ Insta/App.tsx
@@ -4,12 +4,12 @@ import InputBox from './src/components/InputBox'
 import CustomButton from './src/components/CustomButton'
 import Login from './src/screen/auth/Login'
 import Signup from './src/screen/auth/Signup'
+import StackNavigation from './src/navigator/StackNavigation'
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      {/* <Login /> */}
-      <Signup />
+      <StackNavigation />
     </View>
   )
 }
