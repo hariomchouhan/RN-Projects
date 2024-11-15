88b43e899ec0970f97f564e39337897d57279c47
Hariom <hariomchouhan430@gmail.com>
2024-11-15 18:37:47 +1030

    Complete the Profile Post Section in UserProfile Screen

--- Insta/src/components/ProfilePost.jsx
+++ Insta/src/components/ProfilePost.jsx
@@ -1,12 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Dimensions, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { typeData, UserData } from '../utils/UserData'
 
 const ProfilePost = () => {
-  return (
-    <View style={styles.mainContainer}>
-        <View style={styles.wrapper}></View>
-    </View>
-  )
+    const screenWidth = Dimensions.get('window').width
+    const [selected, setSelected] = useState(1)
+
+    const renderItem = item => {
+        return(
+            <View style={{flex: 1, alignItems: 'center'}}>
+                <Image source={item.item.post.image} style={{height: screenWidth/3, width: screenWidth/3, borderWidth: 0.2}} />
+            </View>
+        )
+    }
+    return (
+        <View style={styles.mainContainer}>
+            <View style={styles.wrapper}>
+                {typeData?.map((item) => (
+                    <View key={item.id} style={{ paddingBottom: 15, width: screenWidth / 2, borderBottomWidth: selected === item.id ? 1 : 0 }}>
+                        <TouchableOpacity onPress={() => setSelected(item.id)}>
+                            <Image source={item.image} style={{ tintColor: 'black', alignSelf: 'center' }} />
+                        </TouchableOpacity>
+                    </View>
+                ))}
+            </View>
+            {selected === 1 &&
+            <FlatList
+            data={UserData}
+            renderItem={renderItem}
+            keyExtractor={item => item.id.toString()}
+            numColumns={3}
+            showsHorizontalScrollIndicator={false}
+            />
+            }
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
